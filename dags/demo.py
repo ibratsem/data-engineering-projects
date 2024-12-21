6fc7a5a77f30fe26fdbc81e3c03eb6c1144b5087
ibratsem <ibratsem@gmail.com>
2024-12-22 02:34:01 +0300

    Airflow task. Dmitriy review 1

--- dags/demo.py
+++ dags/demo.py
@@ -1,18 +1,34 @@
 from datetime import datetime
-
+import logging
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
-# A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo", start_date=datetime(2022, 1, 1), schedule="40 * * * *", catchup=False) as dag:
-    # Tasks are represented as operators
-    hello = BashOperator(task_id="hello", bash_command="echo hello", do_xcom_push=True)
+# Set up logging format
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
+# Define the DAG
+with DAG(
+    dag_id="demo",
+    start_date=datetime(2022, 1, 1),  # The start date for the DAG
+    schedule="40 * * * *",  # Run at the 40th minute of every hour
+    catchup=False  # Prevent backfilling
+) as dag:
+    
+    # Define the BashOperator task
+    hello = BashOperator(
+        task_id="hello", 
+        bash_command="echo hello", 
+        do_xcom_push=True  # Enable XCom to push the result of the bash command
+    )
+
+    # Define a Python task using the @task decorator
     @task()
-    def airflow():
-        print("airflow")
+    def airflow(hello_output):
+        logging.info(f"Hello output: {hello_output}")
         return "airflow"
 
-    # Set dependencies between tasks
-    hello >> airflow()
+    # Set the task dependencies
+    hello_output = hello.output  # Capture the output of the `hello` task
+    hello >> airflow(hello_output=hello_output)
+
